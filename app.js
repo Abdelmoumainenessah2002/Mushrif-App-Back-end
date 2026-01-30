e30389ce4197e0df8f98d520c932d7a02adc515b
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-01-30 17:32:59 +0100

    simple updates

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.use(passport.session());
 app.use("/api/auth", require("./routes/authRoute"));
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
