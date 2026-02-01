eb54d6a635cffcc0ba1532939870ab75ae51a12c
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-02-01 18:33:27 +0100

    create new model for verification token when user want to reset his password

---
+++ Models/VerificationToken.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const verificationTokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true
+  },
+
+  token: {
+    type: String,
+    required: true,
+    index: true
+  },
+
+  type: {
+    type: String,
+    enum: ['RESET_PASSWORD', 'VERIFY_EMAIL'],
+    required: true
+  },
+
+  expiresAt: {
+    type: Date,
+    required: true
+  }
+}, { timestamps: true });
+
+// Auto delete after expiration
+verificationTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+module.exports = mongoose.model('VerificationToken', verificationTokenSchema);
