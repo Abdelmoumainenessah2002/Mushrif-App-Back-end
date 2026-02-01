cb0b5641d63c93b5b4782f15152a59276a5dccc0
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-02-01 18:35:12 +0100

    add the password route before pass to the controller

---
+++ routes/passwordRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const {forgotPassword, resetPassword} = require('../controllers/passwordController');
+const forgotPasswordLimiter = require('../middlewares/forgotPasswordLimiter');
+
+router.post(
+  '/forgot',
+  forgotPasswordLimiter,
+  forgotPassword
+);
+
+router.post(
+  '/reset',
+  resetPassword
+);
+
+module.exports = router;
