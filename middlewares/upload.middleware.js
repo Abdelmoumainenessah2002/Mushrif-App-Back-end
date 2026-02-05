6b0ac5528dc13f061f0c3d6258e15cae2fe55e00
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-02-06 00:50:36 +0100

    create middleware for upload the image with multer and set it in buffer

---
+++ middlewares/upload.middleware.js
@@ -0,0 +1,42 @@
+const multer = require('multer');
+const messages = require('../constants/messages');
+const t = require('../utils/t');
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 20 * 1024 * 1024 // 20mb
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith('image/')) {
+      // Store error in request to use language from middleware
+      const error = new Error(t(messages.ONLY_IMAGES_ALLOWED, req.lang || 'en'));
+      error.messageKey = messages.ONLY_IMAGES_ALLOWED;
+      cb(error, false);
+    } else {
+      cb(null, true);
+    }
+  }
+});
+
+// Custom error handler for multer
+const uploadSingleImageWithErrorHandler = (req, res, next) => {
+  upload.single('image')(req, res, (err) => {
+    if (err) {
+      const statusCode = err.code === 'LIMIT_FILE_SIZE' ? 400 : 400;
+      const messageKey = err.code === 'LIMIT_FILE_SIZE' 
+        ? messages.FILE_TOO_LARGE 
+        : (err.messageKey || messages.ONLY_IMAGES_ALLOWED);
+      
+      return res.status(statusCode).json({
+        success: false,
+        message: t(messageKey, req.lang || 'en')
+      });
+    }
+    next();
+  });
+};
+
+module.exports = {
+  uploadSingleImage: uploadSingleImageWithErrorHandler
+};
