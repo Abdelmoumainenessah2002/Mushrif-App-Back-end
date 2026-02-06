77a22c087e6976e2ef67af9366b00c72c93e9f72
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-02-06 20:53:55 +0100

    make the return of multilanguages to the user

--- middlewares/forgotPasswordLimiter.middleware.js
+++ middlewares/forgotPasswordLimiter.middleware.js
@@ -1,12 +1,18 @@
 const rateLimit = require('express-rate-limit');
+const t = require('../utils/t');
+const messages = require('../constants/messages');
 
 const forgotPasswordLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 3,
   standardHeaders: true,
   legacyHeaders: false,
-  message: {
-    message: 'Too many password reset requests from this IP, please try again after 15 minutes'
+  handler: (req, res) => {
+    const lang = req.lang || 'en';
+    res.status(429).json({
+      success: false,
+      message: t(messages.TOO_MANY_EMAILS, lang)
+    });
   }
 });
 
