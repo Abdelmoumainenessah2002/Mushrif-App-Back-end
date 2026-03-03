b6a11b262bbd766b7f878fe08858ed79105338e4
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-03-03 02:39:57 +0100

    add new middlewares

--- routes/passwordRoute.js
+++ routes/passwordRoute.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-const {forgotPassword, resetPassword, validateResetPasswordToken} = require('../controllers/passwordController');
+const {forgotPassword, resetPassword, validateResetPasswordToken, makePasswordForOAuth} = require('../controllers/passwordController');
 const forgotPasswordLimiter = require('../middlewares/forgotPasswordLimiter.middleware');
+const { verifyTokenAndOnlyUser } = require('../middlewares/verifyJWTToken.middleware');
 
+// forgot password
 router.post(
   '/forgot',
   forgotPasswordLimiter,
   forgotPassword
 );
 
+// validate reset password token
+router.get(
+  '/validate/:token',
+  validateResetPasswordToken
+);
+
+// reset password
 router.post(
   '/reset',
   resetPassword
 );
 
-router.get(
-  '/validate/:token',
-  validateResetPasswordToken
+// make password for oauth users
+router.post(
+  '/make-password/:id',
+  verifyTokenAndOnlyUser,
+  makePasswordForOAuth
 );
+
+
 
 module.exports = router;
