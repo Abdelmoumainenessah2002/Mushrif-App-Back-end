47bb9f160b1e4860af0a5b0abbeb9a82cf27256a
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2026-03-03 02:46:29 +0100

    remove the routes of confirm email

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,29 +4,24 @@ const router = express.Router();
 const {
   updateUserProfileCtrl,
   updateUserProfilePhotoCtrl,
-  verifyUserByEmailCtrl,
-  validateEmailVerificationTokenCtrl,
-  verifyEmailAndUpdateUserCtrl,
   suspendUserAccountCtrl,
   unsuspendUserAccountCtrl
 } = require('../controllers/userController');
 
-
 const { uploadSingleImage } = require('../middlewares/upload.middleware');
 const { verifyTokenAndAdmin, verifyTokenAndOnlyUser } = require('../middlewares/verifyJWTToken.middleware');
 
+
+// update user profile
 router.patch('/:id/profile',verifyTokenAndOnlyUser, updateUserProfileCtrl);
 
+// update user profile photo
 router.patch(
   '/:id/profile-photo',
   verifyTokenAndOnlyUser,
   uploadSingleImage,
   updateUserProfilePhotoCtrl
 );
-
-router.post('/:id/verify-email', verifyTokenAndOnlyUser, verifyUserByEmailCtrl);
-router.get('/validate-email/:token', validateEmailVerificationTokenCtrl);
-router.post('/verify-email', verifyEmailAndUpdateUserCtrl);
 
 // suspended and unsuspended routes for admin
 router.put('/suspend/:id', verifyTokenAndAdmin, suspendUserAccountCtrl);
